3577f30eaed677c3c925cb6312904709f54891ad
Antor4q <tariquelislam2015@gmail.com>
2024-04-29 00:16:32 +0600

    finished the single data delete operation

--- index.js
+++ index.js
@@ -83,6 +83,13 @@ async function run() {
       res.send(result)
     })
 
+    app.delete("/myList/:id", async(req,res) => {
+       const id = req.params.id
+       const cursor = { _id : new ObjectId(id)}
+       const result = await spotsCollection.deleteOne(cursor)
+       res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
