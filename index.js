a89d5e59239ad63c794eef94464d8a90f798fcca
Antor4q <tariquelislam2015@gmail.com>
2024-04-30 15:51:17 +0600

    read data by country name operaton finished

--- index.js
+++ index.js
@@ -48,6 +48,12 @@ async function run() {
       res.send(result)
     })
 
+    app.get("/countryData/:countryName",async(req,res) => {
+        const cursor = req.params.countryName
+        const result = await spotsCollection.find({countryName : cursor}).toArray()
+        res.send(result)
+    })
+
     app.get("/touristSpots/:id", async (req,res) => {
       const id = req.params.id;
       const cursor = { _id : new ObjectId(id)}
